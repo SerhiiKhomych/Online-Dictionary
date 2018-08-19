ea86efb2b18990e1eaaa36bbe187e410fab99b83
serhii <sergeykhomich5@gmil.com>
2018-08-19 09:36:42 +0300

    Code clean-up

--- src/main/java/com/study/Application.java
+++ src/main/java/com/study/Application.java
@@ -2,14 +2,8 @@ package com.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
